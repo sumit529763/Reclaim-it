0b5737b6ddbd76ec45b787c96da04cb821cda978
Demo <naiks0234@gmail.com>
2025-08-16 18:33:11 +0530

    added cover.png

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Features from "./components/Features";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+// hello
 
 function App() {
   return (
